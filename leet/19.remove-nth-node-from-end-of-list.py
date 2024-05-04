5c943e853df193741a569c361d9f589eb2a2750c
John Tracy <58991818+jwtracy@users.noreply.github.com>
2024-05-03 17:05:22 -0700

    some more code

--- leet/19.remove-nth-node-from-end-of-list.py
+++ leet/19.remove-nth-node-from-end-of-list.py
@@ -6,12 +6,35 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        stack = []
+        cur = head
+        while cur:
+            stack.append(cur)
+            cur = cur.next
+
+        nthLastIndex = len(stack) - n
+        print(nthLastIndex)
+        if nthLastIndex == 0:
+            print("here")
+            return head.next
+        elif nthLastIndex > 0:
+            print("there")
+            stack[nthLastIndex - 1].next = stack[nthLastIndex].next
+            return head
+        else:
+            print("everywhere")
+            return None
+
+
+
+
+        
         
 # @lc code=end
 
--- leet/42.trapping-rain-water.py
+++ leet/42.trapping-rain-water.py
@@ -7,6 +7,26 @@
 # @lc code=start
 class Solution:
     def trap(self, height: List[int]) -> int:
+        volumetric_grid = height.copy()
+        left_max = 0
+        for i in range(len(height)):
+            left_max = max(left_max, height[i])
+            volumetric_grid[i] = left_max
+
+        water = 0
+        right_max = 0
+        for i in range(len(height)):
+            elevation = height[len(height) - (i + 1)]
+            right_max = max(right_max, elevation)
+            if volumetric_grid[len(height) - (i + 1)] > right_max:
+                volumetric_grid[len(height) - (i + 1)] = right_max
+            water += volumetric_grid[len(height) - (i + 1)] - height[len(height) - (i + 1)]
+
+        return water
+
+
+
+            
         
 # @lc code=end
 
